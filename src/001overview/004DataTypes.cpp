9f134cbafe3fd681d126854dd3fca854c0a98848
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-02 23:40:42 +0530

    Add DataTypes.cpp file

---
+++ src/001overview/004DataTypes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a=1;
+    int o=051;
+    int x=0x2a;
+    int b=0b101010;
+    unsigned int u_1=42u;
+    long int l=123456789123;
+    float f=1.5;
+    double d=1.5631477899546;
+    bool boo=true;
+    cout << "Print\ninteger decimal a=" << a << "\ninteger octal o=" << o << "\ninteger hexadecimal x=" <<x<< "\ninteger binary b="<<b<<"\nunsigned integer u_1="<<u_1<<"\nlong integer l="<<l<<"\nfloating point f="<<f<<"\ndouble d="<<d<<"\nboolean boo="<<boo << endl;
+}
