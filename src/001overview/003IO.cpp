0f6d5660ff128b2020bc84a3be73a3da482d3ad3
Nilesh Medda <meddanilesh7@gmail.com>
2026-02-02 22:46:59 +0530

    Add 003IO.cpp file

---
+++ src/001overview/003IO.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    int a; // 'a' is an integer type variable
+    std::cout << "Enter a number: " << std::endl;
+    std::cin >> a;
+    std::cout << "The given number is : " << a << std::endl;
+}
